c61ee3c3a4fbe1515f2ff05608768f6b49be735e
Jared Yeung <130919853+jaredycw@users.noreply.github.com>
2025-03-05 16:25:16 +0800

    Update page.tsx

--- app/(site)/contact/page.tsx
+++ app/(site)/contact/page.tsx
@@ -43,6 +43,8 @@ export default function ContactMe(){
                         </div>
                        <p>Feel free to contact me 🔥</p>
                         <p>chat, copyrights, technical issues, opportunities, freelance services...</p>
+                        <p>Need a stunning, mobile-friendly website or blog? Let's try <a href="https://www.strikingly.com/a/NaRgIB" target="_blank">Strikingly</a>, a user-friendly platform that lets anyone—coding skills or not—build sleek, optimized sites for all devices, 
+                           from phones to desktops. Get in touch, and let’s create something amazing together!</p>
                     </div>
                     <div className="mb-10">
                         <span className="icon-email mr-2"></span>
