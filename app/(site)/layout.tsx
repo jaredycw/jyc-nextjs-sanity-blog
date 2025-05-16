0764078221b1163ddd65db847081ffef507275ab
Jared Yeung <130919853+jaredycw@users.noreply.github.com>
2025-05-16 14:23:32 +0800

    Update layout.tsx

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -34,9 +34,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+      <meta name="google-adsense-account" content="ca-pub-4866270005296040">  
       <link rel="stylesheet" href="https://use.typekit.net/alm4ysv.css"></link> 
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4866270005296040"
-       crossorigin="anonymous"></script>
       </head>
       <body>
         <Header />
