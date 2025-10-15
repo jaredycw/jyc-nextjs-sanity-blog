c3ffb54be281d13415b53636dbfb5f6325c1360f
Jared Yeung <130919853+jaredycw@users.noreply.github.com>
2025-10-15 10:54:15 +0800

    Update moviereviewspart.tsx

--- app/component/books/moviereviewspart.tsx
+++ app/component/books/moviereviewspart.tsx
@@ -48,7 +48,7 @@ export default function MovieReviewsPart(){
                   </Link>
                   ))
                 ) : (
-                  <>Failed</>
+                  <>Failed to fetch data</>
               )}
               </div>
             </div>
